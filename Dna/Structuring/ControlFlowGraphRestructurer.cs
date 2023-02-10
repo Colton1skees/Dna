e4a15bfc5ec91cde1e15aff6061f14225d7edc99
Colton1skees <Colton1skees@gmail.com>
2023-02-09 19:00:45 -0600

    refine SCS regions

--- Dna/Structuring/ControlFlowGraphRestructurer.cs
+++ Dna/Structuring/ControlFlowGraphRestructurer.cs
@@ -1,4 +1,5 @@
 ﻿using Dna.ControlFlow;
+using Dna.ControlFlow.Analysis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,51 @@ namespace Dna.Structuring
         public ControlFlowGraphRestructurer(ControlFlowGraph<T> graph)
         {
             this.graph = graph;
+        }
+
+        public void Restructure()
+        {
+            // Create the initial set of SCS regions via identifying all back edges.
+            var backEdges = GetScsRegions();
+
+            // Refine the SCS regions.
+            RefineScsRegions(backEdges);
+        }
+
+
+        private HashSet<BlockEdge<T>> GetScsRegions()
+        {
+            // Gets all outgoing edges where the target dominates its source.
+            var scsRegions = BackEdgeAnalysis.GetBackEdges(graph).Cast<BlockEdge<T>>();
+
+            // Cast the set of 
+            return new HashSet<BlockEdge<T>>(scsRegions);
+        }
+
+        private void RefineScsRegions(HashSet<BlockEdge<T>> backEdges)
+        {
+            // Insert a dummy node for each retreating edge.
+            // TODO: Add code to JMP to the correct destination.
+            foreach(var backEdge in backEdges)
+            {
+                var target = backEdge.TargetBlock;
+                var dummy = NodeInserter.InsertArtificalNode(graph);
+                EdgeUpdater.ReplaceTarget(backEdge, dummy);
+                EdgeUpdater.AddEdge(dummy, target);
+            }
+
+            // Recompute back edges.
+            backEdges = GetScsRegions();
+
+            // Assert that the source of each retreating edge is a dummy node.
+            if (backEdges.Any(x => x.SourceBlock.Instructions.Any()))
+                throw new InvalidOperationException("The source node of a retreating edge must now always be a dummy node.");
+
+        }
+
+        private void CreateMetaRegions(HashSet<BlockEdge<T>> backEdges)
+        {
+
         }
     }
 }
---
+++ Dna/Structuring/NodeInserter.cs
@@ -0,0 +1,21 @@
+﻿using Dna.ControlFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dna.Structuring
+{
+    public class NodeInserter
+    {
+        public static BasicBlock<T> InsertArtificalNode<T>(ControlFlowGraph<T> graph)
+        {
+            // Allocate the block at an arbitrary address. TODO: Refactor.
+            var artificialAddress = graph.GetBlocks().MaxBy(x => x.Address).Address + 0x1000;
+
+            var block = graph.CreateBlock(artificialAddress);
+            return block;
+        }
+    }
+}
