12b93471fe4a014349bdb0009f1792eb382a2cf2
Colton1skees <Colton1skees@gmail.com>
2023-02-09 18:00:40 -0600

    Implement back edge analysis

---
+++ Dna.ControlFlow/Analysis/BackEdgeAnalysis.cs
@@ -0,0 +1,29 @@
+﻿using Rivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dna.ControlFlow.Analysis
+{
+    public static class BackEdgeAnalysis
+    {
+        /// <summary>
+        /// Gets all outgoing edges where the target dominates its source.
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="dominatorTree"></param>
+        /// <returns></returns>
+        public static HashSet<Edge> GetBackEdges(Graph graph, IReadOnlyDictionary<Node, HashSet<Node>> dominatorTree)
+        {
+            // Collect all outgoing edges where node {S} is jumping to node {T} which dominates {S}.
+            var backEdges = graph.Nodes
+                .SelectMany(node => node.OutgoingEdges)
+                .Where(outEdge => dominatorTree[outEdge.Source].Contains(outEdge.Target));
+
+            // Return a set of all back edges.
+            return new HashSet<Edge>(backEdges);
+        }
+    }
+}
