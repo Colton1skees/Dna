f045e749f05ac741b0f271d2f3f3e37e4772f8bd
Colton1skees <Colton1skees@gmail.com>
2023-03-03 14:32:41 -0600

    Cleanup Dna.Example

--- Dna.Example/Program.cs
+++ Dna.Example/Program.cs
@@ -24,29 +24,27 @@ using Dna.Decompiler;
 using Dna.Emulation.Unicorn;
 using Dna.Decompilation;
 using Dna.Structuring.Stackify;
-
 // Load the 64 bit PE file.
 // Note: This file is automatically copied to the build directory.
-var path = @"protected_executable.bin";
+var path = @"ProtectedExecutable.bin";
 var binary = new WindowsBinary(64, File.ReadAllBytes(path), 0x140000000);
 
 // Instantiate dna.
 var dna = new Dna.Dna(binary);
 
-// Parse a control flow graph from the binary.
+// Parse a (virtualized) control flow graph from the binary.
 ulong funcAddr = 0x1400F2947;
 var cfg = dna.RecursiveDescent.ReconstructCfg(funcAddr);
 
+// The VM entry spans across multiple routines. To avoid disassembling multiple
+// control flow graphs, we selectively insert instructions needed to have a
+// correct CFG for the entirety of the vm entry.
 var target = cfg.GetBlocks().First();
 target.Instructions.Insert(0, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
 target.Instructions.Insert(1, dna.BinaryDisassembler.GetInstructionAt(0x140001783));
 target.Instructions.Insert(2, dna.BinaryDisassembler.GetInstructionAt(0x140001789));
 target.Instructions.Insert(3, dna.BinaryDisassembler.GetInstructionAt(0x14000178F));
-
-// Here we skip the call.
 target.Instructions.Insert(4, dna.BinaryDisassembler.GetInstructionAt(0x140001794)); 
-
-
 target.Instructions.Insert(5, dna.BinaryDisassembler.GetInstructionAt(0x140002CA0));
 target.Instructions.Insert(6, dna.BinaryDisassembler.GetInstructionAt(0x140002CA5));
 target.Instructions.Insert(7, dna.BinaryDisassembler.GetInstructionAt(0x140002CAA));
@@ -56,16 +54,15 @@ target.Instructions.Insert(10, dna.BinaryDisassembler.GetInstructionAt(0x140002C
 target.Instructions.Insert(11, dna.BinaryDisassembler.GetInstructionAt(0x140019225));
 target.Instructions.Insert(12, dna.BinaryDisassembler.GetInstructionAt(0x14001922A));
 
-/*
-target.Instructions.Insert(13, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
-target.Instructions.Insert(14, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
-target.Instructions.Insert(15, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
-target.Instructions.Insert(16, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
-target.Instructions.Insert(17, dna.BinaryDisassembler.GetInstructionAt(0x14000177E));
-*/
-
+// Print the disassembled control flow graph.
+var prompt = () =>
+{
+    Console.WriteLine("Press enter to continue...");
+    Console.ReadLine();
+};
 Console.WriteLine("Disassembled cfg:\n{0}", GraphFormatter.FormatGraph(cfg));
-//Console.WriteLine(GraphFormatter.FormatGraph(cfg));
+Console.WriteLine(GraphFormatter.FormatGraph(cfg));
+prompt();
 
 // Instantiate the cpu architecture.
 var architecture = new X86CpuArchitecture(ArchitectureId.ARCH_X86_64);
@@ -75,92 +72,88 @@ var cfgLifter = new CfgLifter(architecture);
 
 // Lift the control flow graph to TTIR.
 var liftedCfg = cfgLifter.LiftCfg(cfg);
-// var liftedCfg = StructuringTest.GetSimpleComparison();
 
 for (int i = 0; i < 3; i++)
     Console.WriteLine("");
 
 // Elminate deadcode from the control flow graph.
-//var blockDcePass = new BlockDcePass(liftedCfg);
-//blockDcePass.Run();
+bool dce = false;
+if (dce)
+{
+    var blockDcePass = new BlockDcePass(liftedCfg);
+    blockDcePass.Run();
+}
 
 // Print the optimized control flow graph.
-Console.WriteLine("Optimized cfg:\n{0}", GraphFormatter.FormatGraph(liftedCfg));
+Console.WriteLine("Lifted cfg:\n{0}", GraphFormatter.FormatGraph(liftedCfg));
+prompt();
 
 // Create a .DOT file for visualizing the IR cfg.
-//var dotGraph = GraphVisualizer.GetDotGraph(liftedCfg);
-//File.WriteAllText("graph.dot", dotGraph.Compile(false, false));
+var dotGraph = GraphVisualizer.GetDotGraph(liftedCfg);
+File.WriteAllText("graph.dot", dotGraph.Compile(false, false));
 
-// Load the binary into unicorn engine.
-// var emulator = new UnicornEmulator(architecture);
-//PEMapper.MapBinary(emulator, binary);
+bool emulate = false;
+if(emulate)
+{
+    // Load the binary into unicorn engine.
+    var emulator = new UnicornEmulator(architecture);
+    PEMapper.MapBinary(emulator, binary);
 
-// Setup the stack.
-//ulong rsp = 0x100000000;
-//emulator.MapMemory(rsp, 0x1000 * 12);
-//rsp += 0x100;
-//emulator.SetRegister(register_e.ID_REG_X86_RSP, rsp);
+    // Setup the stack.
+    ulong rsp = 0x100000000;
+    emulator.MapMemory(rsp, 0x1000 * 12);
+    rsp += 0x100;
+    emulator.SetRegister(register_e.ID_REG_X86_RSP, rsp);
 
-// Execute the function.
-//emulator.Start(0x140001747);
+    // Execute the function.
+    emulator.Start(0x140001747);
+}
+
 
 // Lift the control flow graph to LLVM IR.
 var llvmLifter = new LLVMLifter(architecture);
 llvmLifter.Lift(liftedCfg);
+llvmLifter.Module.PrintToFile(@"lifted.ll");
 
-//llvmLifter.Module.Dump();
-
-llvmLifter.Module.PrintToFile(@"C:\Users\colton\Downloads\lifted.ll");
-var exitNodes = liftedCfg.Nodes.Where(x => x.OutgoingEdges.Count == 0);
-
-var passManager = llvmLifter.Module.CreateFunctionPassManager();
-passManager.AddBasicAliasAnalysisPass();
-passManager.AddTypeBasedAliasAnalysisPass();
-passManager.AddScopedNoAliasAAPass();
-
-
-passManager.AddLowerExpectIntrinsicPass();
-passManager.AddCFGSimplificationPass();
-passManager.AddPromoteMemoryToRegisterPass();
-passManager.AddEarlyCSEPass();
-
-passManager.AddDCEPass();
-passManager.AddAggressiveDCEPass();
-passManager.AddDeadStoreEliminationPass();
-
-passManager.AddInstructionCombiningPass();
-passManager.AddCFGSimplificationPass();
-passManager.AddDeadStoreEliminationPass();
-passManager.AddAggressiveDCEPass();
-passManager.InitializeFunctionPassManager();
-for (int i = 0; i < 10; i++)
+bool optimize = true;
+if (optimize)
 {
-    passManager.RunFunctionPassManager(llvmLifter.llvmFunction);
-}
-passManager.FinalizeFunctionPassManager();
+    var passManager = llvmLifter.Module.CreateFunctionPassManager();
+    passManager.AddBasicAliasAnalysisPass();
+    passManager.AddTypeBasedAliasAnalysisPass();
+    passManager.AddScopedNoAliasAAPass();
+    passManager.AddLowerExpectIntrinsicPass();
+    passManager.AddCFGSimplificationPass();
+    passManager.AddPromoteMemoryToRegisterPass();
+    passManager.AddEarlyCSEPass();
+    passManager.AddDCEPass();
+    passManager.AddAggressiveDCEPass();
+    passManager.AddDeadStoreEliminationPass();
+    passManager.AddInstructionCombiningPass();
+    passManager.AddCFGSimplificationPass();
+    passManager.AddDeadStoreEliminationPass();
+    passManager.AddAggressiveDCEPass();
+    passManager.InitializeFunctionPassManager();
+    for (int i = 0; i < 10; i++)
+    {
+        passManager.RunFunctionPassManager(llvmLifter.llvmFunction);
+    }
 
+    passManager.FinalizeFunctionPassManager();
+}
 
 // Optionally write the llvm IR to the console.
 bool printLLVM = true;
 if (printLLVM)
     llvmLifter.Module.Dump();
+prompt();
 
-var llPath = @"C:\Users\colton\Downloads\lifted.ll";
-var compiledAsmPath = @"C:\Users\colton\Downloads\dfgfgfgd\code.asm";
-var wasmTextPath = @"C:\Users\colton\Downloads\dfgfgfgd\code.wat";
-var wasmBinaryPath = @"C:\Users\colton\Downloads\dfgfgfgd\code.wasm";
-var compiledExePath = @"C:\Users\colton\Downloads\dfgfgfgd\code.exe";
-llvmLifter.Module.PrintToFile(llPath);
-llvmLifter.Module.WriteBitcodeToFile(@"C:\Users\colton\Downloads\dfgfgfgd\lifted.bc");
-
-//var test = new StructuringTest();
-//test.Run();
-
-Console.WriteLine("Ran tests... press enter");
-Console.WriteLine("\n\n\n");
-//Console.ReadLine();
-
-bool decompile = true;
+// Optionally decompile the lifted function to go-to free pseudo C, via Rellic.
+// On my machine, a fork of Rellic runs under WSL2 and communiucates via gRPC.
+// If you are not hosting this server at localhost:50051, then the API
+// call will fail. You can find the service here(https://github.com/Colton1skees/rellic-api),
+// although it will take a bit of leg work for outside use.
+bool decompile = false;
 if (decompile)
 {
     // Create a decompiler instance.
@@ -172,52 +165,6 @@ if (decompile)
     // Print the decompiled routine.
     Console.WriteLine("Decompiled routine:\n{0}", ast);
 }
-
-
-var stackifier = new StackifierTwo();
-var wasm = stackifier.Stackify(liftedCfg);
-
-
-var astEmitter = new ASTEmitter();
-astEmitter.Emit(wasm);
-Console.WriteLine(astEmitter.builder.ToString());
-Console.WriteLine();
-
-
-Console.WriteLine("Press enter.");
-Console.ReadLine();
-// Compile the bitcode to a native executable.
-var nativeDecompiler = new LLVMDecompiler();
-//nativeDecompiler.RunClang($"{llPath} -S -o {compiledAsmPath} -O3 -target x86_64");
-//nativeDecompiler.RunClang($"{compiledAsmPath} -target x86_64 -O3 -c -o {compiledExePath}");
-
-// Compile the bitcode to wasm text.
-/*
-nativeDecompiler.RunClang($"{llPath} -o {compiledAsmPath} -O3 -target wasm64");
-var cmd = $"{llPath} -S -o {wasmTextPath} -O3 -target wasm64";
-Console.WriteLine(cmd);
-nativeDecompiler.RunClang(cmd);
-
-// Compile the bitcode to wasm binary.
-var cmd2 = $"wat2wasm {wasmTextPath} -o {wasmBinaryPath}";
-Console.WriteLine(cmd2);
-nativeDecompiler.RunClang(cmd2);
-*/
-
-
-var cmd = $"{llPath} -S -o {wasmTextPath} -O3 -target wasm64";
-//nativeDecompiler.RunClang(cmd);
-//nativeDecompiler.RunClang($"{llPath} -S -o {wasmBinaryPath} -O3 -target wasm64");
-//nativeDecompiler.RunClang($"--target=wasm64 -O3 -nostdlib {llPath} -o {wasmBinaryPath} -Wl,--no-entry");
-// Decompile the bitcode to pseudo C.
-nativeDecompiler.Decompile(wasmBinaryPath);
-Console.WriteLine("Press enter to run rellic.");
-Console.ReadLine();
-// Optionally decompile the lifted function to go-to free pseudo C, via Rellic.
-// On my machine, a fork of Rellic runs under WSL2 and communiucates via gRPC.
-// If you are not hosting this server at localhost:50051, then the API
-// call will fail. You can find the service here(https://github.com/Colton1skees/rellic-api),
-// although it will take a bit of leg work for outside use.
 
 Console.WriteLine("Finished.");
 Console.ReadLine();
